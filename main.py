87b4d3e53f7cd57e4d9f881a53ae5a7513760fde
Reum <rxxxbw19@naver.com>
2022-11-03 15:27:04 +0900

    커밋1

---
+++ main.py
@@ -0,0 +1,10 @@
+from add import add_func
+from mul import mul_func
+
+num1, num2, res=100, 200, 0
+
+res=add_func(num1, num2)
+print(num1, "+", num2, "=", res)
+
+res=mul_func(num1, num2)
+print(num1, "*", num2, "=", res)
